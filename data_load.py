f61aaa94a066b1fb1c0b83f7e5cb2f3ec966201f
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2018-08-01 17:46:11 -0500

    Formatting.

--- data_load.py
+++ data_load.py
@@ -1,11 +1,11 @@
-import glob
 import os
-import torch
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
-import matplotlib.image as mpimg
-import pandas as pd
+
 import cv2
+import matplotlib.image as mpimg
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
 
 
 class FacialKeypointsDataset(Dataset):
@@ -30,14 +30,14 @@ class FacialKeypointsDataset(Dataset):
 
     def __getitem__(self, idx):
         image_name = os.path.join(self.root_dir,
-                                self.key_pts_frame.iloc[idx, 0])
-        
+                                  self.key_pts_frame.iloc[idx, 0])
+
         image = mpimg.imread(image_name)
-        
+
         # if image has an alpha color channel, get rid of it
         if image.shape[2] == 4:
-            image = image[:,:,0:3]
-        
+            image = image[:, :, 0:3]
+
         key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
         key_pts = key_pts.astype('float').reshape(-1, 2)
         sample = {'image': image, 'keypoints': key_pts}
@@ -46,35 +46,33 @@ class FacialKeypointsDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-    
 
-    
+
 # Tranforms
 
 class Normalize(object):
     """
     Convert a color image to grayscale and normalize the color range to [0,1].
-    """        
+    """
 
     def __call__(self, sample):
-        image, key_pts = sample['image'], sample['keypoints']
-        
-        image_copy = np.copy(image)
+        image = sample['image']
+        key_pts = sample['keypoints']
+
         key_pts_copy = np.copy(key_pts)
 
         # convert image to grayscale
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
+
         # scale color range from [0, 255] to [0, 1]
-        image_copy=  image_copy/255.0
-            
-        
+        image_copy = image_copy / 255.0
+
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-        key_pts_copy = (key_pts_copy - 100)/50.0
+        key_pts_copy = (key_pts_copy - 100) / 50.0
 
-
-        return {'image': image_copy, 'keypoints': key_pts_copy}
+        return {'image': image_copy,
+                'keypoints': key_pts_copy}
 
 
 class Rescale(object):
@@ -94,23 +92,26 @@ class Rescale(object):
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
 
-        h, w = image.shape[:2]
+        height, width = image.shape[:2]
         if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
+            if height > width:
+                new_height = self.output_size * height / width
+                new_width = self.output_size
             else:
-                new_h, new_w = self.output_size, self.output_size * w / h
+                new_height = self.output_size
+                new_width = self.output_size * width / height
         else:
-            new_h, new_w = self.output_size
+            new_height, new_width = self.output_size
 
-        new_h, new_w = int(new_h), int(new_w)
+        new_height, new_width = int(new_height), int(new_width)
 
-        img = cv2.resize(image, (new_w, new_h))
-        
+        rescaled_image = cv2.resize(image, (new_width, new_height))
+
         # scale the pts, too
-        key_pts = key_pts * [new_w / w, new_h / h]
+        key_pts = key_pts * [new_width / width, new_height / height]
 
-        return {'image': img, 'keypoints': key_pts}
+        return {'image': rescaled_image,
+                'keypoints': key_pts}
 
 
 class RandomCrop(object):
@@ -124,6 +125,7 @@ class RandomCrop(object):
 
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
+
         if isinstance(output_size, int):
             self.output_size = (output_size, output_size)
         else:
@@ -131,20 +133,21 @@ class RandomCrop(object):
             self.output_size = output_size
 
     def __call__(self, sample):
-        image, key_pts = sample['image'], sample['keypoints']
+        image = sample['image']
+        key_pts = sample['keypoints']
 
-        h, w = image.shape[:2]
-        new_h, new_w = self.output_size
+        height, width = image.shape[:2]
+        new_height, new_width = self.output_size
 
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
+        top = np.random.randint(0, height - new_height)
+        left = np.random.randint(0, width - new_width)
 
-        image = image[top: top + new_h,
-                      left: left + new_w]
+        image = image[top: top + new_height, left: left + new_width]
 
         key_pts = key_pts - [left, top]
 
-        return {'image': image, 'keypoints': key_pts}
+        return {'image': image,
+                'keypoints': key_pts}
 
 
 class ToTensor(object):
@@ -153,17 +156,18 @@ class ToTensor(object):
     """
 
     def __call__(self, sample):
-        image, key_pts = sample['image'], sample['keypoints']
-         
+        image = sample['image']
+        key_pts = sample['keypoints']
+
         # if image has no grayscale color channel, add one
-        if(len(image.shape) == 2):
+        if len(image.shape) == 2:
             # add that third color dim
             image = image.reshape(image.shape[0], image.shape[1], 1)
-            
+
         # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
+        # numpy image: Height x Width x Channels
+        # torch image: Channels X Height X Width
         image = image.transpose((2, 0, 1))
-        
+
         return {'image': torch.from_numpy(image),
                 'keypoints': torch.from_numpy(key_pts)}
